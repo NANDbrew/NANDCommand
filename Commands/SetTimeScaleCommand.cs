0aade543f95aa81944eb348779715c105e380843
NANDbrew <zortman@gmail.com>
2025-06-08 11:52:04 -0500

    SetTimeScale now supports scale as a ratio

--- Commands/SetTimeScaleCommand.cs
+++ Commands/SetTimeScaleCommand.cs
@@ -15,19 +15,28 @@ namespace NANDCommand.Commands
     {
         public override string Name => "setTimeScale";
 
-        public override string Usage => "[float time]";
+        public override string Usage => "[float time] [use ratio (-r)]";
 
-        public override string Description => "Sets day/night time scale. if unspecified, resets to default (0.008)";
+        public override string Description => "Sets day/night time scale. if unspecified, resets to default (0.008 aka 28.8)";
 
         public override void OnRun(List<string> args)
         {
             float scale = 0.008f;
-            if (args.Count == 1)
+            if (args.Count >= 1)
             {
+                if (!char.IsNumber(args[0][0]))
+                {
+                    ModConsoleLog.Error(Plugin.instance.Info, $"invalid multiplier");
+                    return;
+                }
                 scale = Convert.ToSingle(args[0]);
             }
+            if (args.Count > 1 && args.Last().ToLower() == "-r")
+            {
+                scale /= 3600;
+            }
             Sun.sun.initialTimescale = scale;
-            ModConsoleLog.Log(Plugin.instance.Info, $"set time scale to {scale}");
+            ModConsoleLog.Log(Plugin.instance.Info, $"setting time scale to {scale} aka {scale * 3600}");
 
         }
 
