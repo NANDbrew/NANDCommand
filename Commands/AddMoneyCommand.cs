252f5a06074ece2e8625ddd66cee5b8006b8dcc2
NANDbrew <zortman@gmail.com>
2026-05-13 21:01:58 -0500

    added AddMoney command: works like AddGold, but accepts negative values

---
+++ Commands/AddMoneyCommand.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using SailwindConsole;
+using SailwindConsole.Commands;
+using UnityEngine;
+
+namespace NANDCommand.Commands
+{
+    public class AddMoneyCommand : Command
+    {
+        public override string Name => "AddMoney";
+        public override int MinArgs => 2;
+
+        public override string Usage => "<currency type (0-3)> <amount>";
+
+        public override string Description => "Add or subtract gold";
+
+        public override void OnRun(List<string> args)
+        {
+            int.TryParse(args[0], out var result);
+            int.TryParse(args[1], out var result2);
+            if (result < 0 || result > 3)
+            {
+                ModConsoleLog.Error(Plugin.instance.Info, "Currency type out of range");
+            }
+            else
+            {
+                PlayerGold.currency[result] += result2;
+                UISoundPlayer.instance.PlayGoldSound();
+                ModConsoleLog.Log(Plugin.instance.Info, $"Changed {PlayerGold.GetCurrencyName(result)} by {result2}");
+            }
+
+        }
+
+    }
+}
