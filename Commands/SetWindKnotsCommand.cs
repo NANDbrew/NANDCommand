142ee690d111d44fd683ef1effceb94cf4f4eab2
NANDbrew <zortman@gmail.com>
2026-07-21 00:06:24 -0500

    SetWindKnots will take a floating point now. also added a multiplier to make it match chiplog knots

--- Commands/SetWindKnotsCommand.cs
+++ Commands/SetWindKnotsCommand.cs
@@ -8,15 +8,15 @@ namespace NANDCommand.Commands
     {
         public override string Name => "setWindKnots";
         public override int MinArgs => 1;
-        public override string Usage => "<wind speed (int knots)>";
+        public override string Usage => "<wind speed (knots)>";
 
-        public override string Description => "Set wind speed";
+        public override string Description => "Set wind speed for 90 seconds";
 
         public override void OnRun(List<string> args)
         {
-            if (int.TryParse(args[0], out int windSpeed))
+            if (float.TryParse(args[0], out float windSpeed))
             {
-                Wind.instance.SetPrivateField("currentWindTarget", Wind.currentBaseWind.normalized * windSpeed);
+                Wind.instance.SetPrivateField("currentWindTarget", Wind.currentBaseWind.normalized * (windSpeed / 1.865f));
                 Wind.instance.SetPrivateField("timer", 90);
                 ModConsoleLog.Log(Plugin.instance.Info, "Wind speed set!");
             }
