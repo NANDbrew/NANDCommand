10add41ca8f019f8390a92cfa681658426a29421
NANDbrew <zortman@gmail.com>
2026-08-04 18:26:17 -0500

    updated MoveBoatCommand to use BoatMover

--- Commands/MoveBoatCommand.cs
+++ Commands/MoveBoatCommand.cs
@@ -19,6 +19,16 @@ namespace NANDCommand.Commands
 
         public override void OnRun(List<string> args)
         {
+            if (GameState.recovering)
+            {
+                ModConsoleLog.Error(Plugin.instance.Info, "can't move; already moving");
+                return;
+            }
+/*            if (FloatingOriginManager.ShiftingThisFrame)
+            {
+                ModConsoleLog.Error(Plugin.instance.Info, "can't move; already moving");
+                return;
+            }*/
             float longitude;
             float latitude;
             if (args[0].ToLower() == "random" && args[1].ToLower() == "location")
@@ -31,95 +41,53 @@ namespace NANDCommand.Commands
                 longitude = Convert.ToSingle(args[1]);
                 latitude = Convert.ToSingle(args[0]);
             }
-            bool bringNearby = args.Count > 2 && args.Last().ToLower() == "-y";
-            //Transform boat = BoatFinder.FindBoat(args.Count > 2? args[2] : "");
-            Transform boat = args.Count > 2 ? BoatFinder.FindBoat(args[2]) : BoatFinder.FindBoat();
+            Transform boat = BoatFinder.FindBoat();
+            bool bringNearby = (args.Count > 2 && args.Last().ToLower() == "-y");
+            if (args.Count > 2)
+            {
+                if (args[2].ToLower() != "-y")
+                {
+                    boat = BoatFinder.FindBoat(args[2]);
+                } 
+            }
 
             if (boat == null)
             {
                 ModConsoleLog.Error(Plugin.instance.Info, "couldn't find a boat to move");
                 return;
             }
-            FloatingOriginManager.instance.StartCoroutine(MoveBoatToGlobeCoords(longitude, latitude, bringNearby, boat));
-            ModConsoleLog.Log(Plugin.instance.Info, $"moved boat {boat.name} to {latitude}, {longitude}");
-
-        }
-
-        public static IEnumerator MoveBoatToGlobeCoords(float x, float z, bool bringNearby, Transform boat)
-        {
-            boat.GetComponent<PurchasableBoat>().LoadAsPurchased();
-            boat.GetComponent<BoatDamage>().waterLevel = 0;
-
-            bool movingPlayer = GameState.currentBoat;
-            if (movingPlayer) GameState.recovering = true;
-
-            Dictionary<PickupableBoatMooringRope, GameObject> ropeMoorings = new Dictionary<PickupableBoatMooringRope, GameObject>();
 
             Vector3 globeOffset = (Vector3)Traverse.Create(FloatingOriginManager.instance).Field("globeOffset").GetValue();
-            Vector3 targetPos = new Vector3(x, 0f, z) * 9000 + globeOffset + Vector3.up * 5;
-
-            BoatMooringRopes ropes = boat.GetComponent<BoatMooringRopes>();
-            ropes.GetAnchorController().ResetAnchor();
+            Vector3 targetPos = new Vector3(longitude, 0f, latitude) * 9000 + globeOffset + Vector3.up * 5;
+            targetPos = FloatingOriginManager.instance.RealPosToShiftingPos(targetPos);
 
             PurchasableBoat[] nearbyBoats = new PurchasableBoat[0];
             Vector3[] relVectors = new Vector3[0];
             if (bringNearby)
             {
-                // Record nearby boat positions relative to main boat
-                nearbyBoats = GameObject.FindObjectsOfType<PurchasableBoat>().Where(o => (o.transform != boat && o.isPurchased() && (o.transform.position - GameState.lastBoat.transform.position).sqrMagnitude < 10000)).ToArray();
+                nearbyBoats = GameObject.FindObjectsOfType<PurchasableBoat>().Where(o => (o.transform != boat && o.isPurchased() && (o.transform.position - boat.position).sqrMagnitude < 10000)).ToArray();
                 relVectors = new Vector3[nearbyBoats.Length];
                 for (int i = 0; i < nearbyBoats.Length; i++)
                 {
-                    var nearBoat = nearbyBoats[i];
-                    var NBropeSet = nearBoat.GetComponent<BoatMooringRopes>();
-                    NBropeSet.GetAnchorController().ResetAnchor();
-                    foreach (var rope in NBropeSet.ropes)
-                    {
-                        if (rope.IsMoored() && !rope.transform.parent.CompareTag("Boat"))
-                        {
-                            rope.Unmoor();
-                            rope.ResetRopePos();
-                        }
-                    }
-                    relVectors[i] = nearBoat.transform.position - boat.position;
+                    relVectors[i] = nearbyBoats[i].transform.position - boat.position;
                 }
-
-                foreach (var rope in ropes.ropes)
-                {
-                    if (rope.IsMoored() && !rope.transform.parent.CompareTag("Boat"))
-                    {
-                        rope.Unmoor();
-                        rope.ResetRopePos();
-                    }
-                }
-            }
-            else
-            {
-                ropes.UnmoorAllRopes();
             }
 
-            // Move main boat and set safe rotation
-            boat.position = FloatingOriginManager.instance.RealPosToShiftingPos(targetPos);
-            boat.eulerAngles = new Vector3(0f, boat.eulerAngles.y, 0f);
-            if (bringNearby)
+            FloatingOriginManager.instance.StartCoroutine(BoatMover.IMoveBoat(targetPos, boat.rotation, boat));
+            ModConsoleLog.Log(Plugin.instance.Info, $"moving boat {boat.name} to {latitude}, {longitude}...");
+
+            if (bringNearby && nearbyBoats.Length > 0 && relVectors.Length > 0)
             {
-                // Move nearby boats
-                for (int i = 0; i < nearbyBoats.Length; i++)
+                for (int j = 0; j < nearbyBoats.Length; j++)
                 {
-                    var nearBoat = nearbyBoats[i];
-                    nearBoat.GetComponent<BoatDamage>().waterLevel = 0;
-                    nearBoat.transform.position = relVectors[i] + boat.position;
-                    nearBoat.transform.eulerAngles = new Vector3(0f, nearBoat.transform.eulerAngles.y, 0f);
-                    ModConsoleLog.Log(Plugin.instance.Info, $"moved nearby boat {nearBoat.name}");
+                    Transform nb = nearbyBoats[j].transform;
+                    FloatingOriginManager.instance.StartCoroutine(BoatMover.IMoveBoat(targetPos + relVectors[j], nb.transform.rotation, nb));
+                    ModConsoleLog.Log(Plugin.instance.Info, $"moving boat {nb.name}...");
+
                 }
             }
-            if (movingPlayer)
-            {
-                RefsDirectory.instance.oceanRenderer.enabled = false;
-                yield return new WaitUntil(() => !GameState.wasInSettingsMenu);
-                GameState.recovering = false;
-                RefsDirectory.instance.oceanRenderer.enabled = true;
-            }
+
         }
+
     }
 }
