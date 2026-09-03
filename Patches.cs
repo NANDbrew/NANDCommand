6a9f3ffb1cf2cb472d5fca5c49515889b1e822b2
NANDbrew <zortman@gmail.com>
2026-09-02 21:32:54 -0500

    revert text log limiter to uglier, more reliable method

--- Patches.cs
+++ Patches.cs
@@ -36,8 +36,8 @@ namespace NANDCommand
                 if (___logText.text.Length > 20000)
                 {
                     int amount = ___logText.text.Length - 20000;
-                    //___logText.text = "..." + ___logText.text.Remove(0, amount);// ___logText.text.IndexOf(Environment.NewLine, amount));
-                    ___logText.text = ___logText.text.Remove(0, ___logText.text.IndexOf(Environment.NewLine, amount));
+                    ___logText.text = "..." + ___logText.text.Remove(0, amount);// ___logText.text.IndexOf(Environment.NewLine, amount));
+                    //___logText.text = ___logText.text.Remove(0, ___logText.text.IndexOf(Environment.NewLine, amount));
                     Debug.Log($"NANDCommand: trimmed {amount} chars off mod console log");
                 }
             }
